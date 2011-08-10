1ccef00fdc5990c42bf9a5fe82a2234fa9b3ce15
Elcida <Elcida@192.168.2.9>
2011-08-10 00:00:23 -0430

    se creo la clase Automata

---
+++ src/automatas/Automata.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automatas;
+
+/**
+ *
+ * @author Elcida
+ */
+public class Automata {
+    
+}
