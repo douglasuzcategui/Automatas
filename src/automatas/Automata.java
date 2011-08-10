d16b2fc95a0161a4aaa1eecb0be1010f1805e2c7
Elcida <Elcida@192.168.2.9>
2011-08-10 00:02:58 -0430

    se creo la clase Automata

--- src/automatas/Automata.java
+++ src/automatas/Automata.java
@@ -4,10 +4,36 @@
  */
 package automatas;
 
+import java.io.*;
+import java.util.*;
+
 /**
  *
  * @author Elcida
  */
 public class Automata {
     
+    public static void cargardatos(String archivo) throws FileNotFoundException{
+        
+        Scanner leer = new Scanner(new File(archivo));
+        
+        String estado_ini = leer.nextLine();
+        System.out.println(estado_ini);
+        
+        String aux_edo = leer.nextLine();
+        String[] estado_aceptador = aux_edo.split(" ");
+        
+        String aux_alf = leer.nextLine();
+        String[] alfabeto = aux_alf.split(" ");
+        
+        
+        
+        System.out.println(estado_aceptador[0]+" "+ estado_aceptador[1]);
+        System.out.println(alfabeto[0]+" "+ alfabeto[1]);
+        
+        
+        
+        /*String alfabeto = leer.nextLine();
+        System.out.println(estado_aceptador);*/
+    }
 }
